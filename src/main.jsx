46d380a565a3b9f5b672629a0997c89634ff391c
Mark Stanik <mark.stanik@gmail.com>
2024-01-14 19:27:49 -0500

    updated useEffect()

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,20 @@ import './index.css';
 
 function Main() {
   const [players, setPlayers] = useState([]);
+  const [table, setTable] = useState([]);
 
   useEffect(() => {
     // Add a cache-busting query parameter
     fetch('/api?_=' + new Date().getTime())
       .then(response => response.json())
-      .then(data => setPlayers(data))
+      .then(data => {
+        const relevantData = data[1]; // Access the second index of the array
+        const tableData = data[0]
+        console.log(relevantData); // Print the relevant data
+        setPlayers(relevantData); // Use the relevant data to set the state
+        console.log(tableData);
+        setTable(tableData)
+      })
       .catch(error => console.error('Error:', error));
   }, []);
 
